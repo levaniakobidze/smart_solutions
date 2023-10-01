e5da6efc5b64a966f35d6359e4d95346baa292e2
levaniakobidze <levaniakobidze25@gmail.com>
2023-10-01 19:05:58 +0400

    fix: simple fix to host app

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {
   ColumnDef,
